c2891787db5de819603489c3b6f48af4d263b01c
sara hajaj <113361965+SaraHajaj@users.noreply.github.com>
2022-11-02 23:28:02 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,43 @@
+# For Hacktoberfest
+## Notes
+* Contact here mujahidali88094@gmail.com with 'hacktoberfest_help' in title.
+* Please first visit https://mujahidali88094.github.io/React_Budgets_App/ to see it working.
+* Do read this https://www.freecodecamp.org/news/how-to-contribute-to-open-source-projects-beginners-guide/#howtosubmitapullrequest if it is your first time.
+* Don't forget to run **npm install** after downloading the code and before running **npm start**.
+* Do checkout https://github.com/mujahidali88094/React_Budgets_App/edit/main/README.md#possible-additions section if you are unable to identify a possible change.
+## Possible additions
+* Live Search Budgets Feature (search results update as one types in the searchbar)
+* Change Limit of A Budget
+   (in case of decreasing limit, one can move the exceeding expenses to uncategorized section)
+* use of other hooks (use case is one's imagination)
+* recent expenses overall and for each budget
+* expenses graph
+# Description
+This project manages budgets and expenses. You can add a budget and set it's limit. Then you can add expenses to a budget and track your pocket.
+# Technical Details
+This project was aimed to use as many react hooks as possible.
+Currently it uses:
+  * useState
+  * useEffect
+  * useRef
+  * useContext
+  * useLocalStorage (custom hook to sustain data between page-refreshes)
+  * useBudgetsContextValue (just a wrapper above useContext to get context_value)
+# Link to Deployed App
+https://mujahidali88094.github.io/React_Budgets_App/
+# Getting Started with Create React App
+
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
+## Available Scripts
+
+In the project directory, you can run:
+
+### `npm start`
+
+Runs the app in the development mode.\
+Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+
+The page will reload when you make changes.\
+You may also see any lint errors in the console.
+# reactjs
---
+++ src/components/BudgetCard.js
@@ -0,0 +1,84 @@
+import { Modal, Button, Stack, Card } from "react-bootstrap";
+import {
+  UNCATEGORIZED_ID,
+  useBudgetsContextValue,
+} from "../contexts/BudgetsContext";
+
+export default function BudgetCard({
+  name,
+  max,
+  current,
+  hideButtons,
+  onAddExpenseClick,
+  onViewExpensesClick,
+  budgetId,
+}) {
+  let progress = 0,
+    colorClass = "";
+  if (max != undefined && max != null && max != 0) {
+    progress = Math.ceil((current / max) * 100);
+    if (progress <= 50) colorClass = "bg-success";
+    else if (progress <= 75) colorClass = "bg-warning";
+    else colorClass = "bg-danger";
+  }
+
+  const { removeBudget } = useBudgetsContextValue();
+
+  return (
+    <div className="card my-1">
+      <div className="card-body">
+        <div className="card-title d-flex justify-content-between font-weight-bold align-items-baseline">
+          <h5>{name}</h5>
+          <div>
+            ${current}
+            {max != undefined && max != null && (
+              <span className="text-muted">/${max}</span>
+            )}
+          </div>
+        </div>
+        {max != undefined && max != null && (
+          <div className="progress my-3">
+            <div
+              className={"progress-bar " + colorClass}
+              role="progressbar"
+              aria-valuenow={progress}
+              aria-valuemin="0"
+              aria-valuemax="100"
+              style={{ width: progress + "%" }}
+            ></div>
+          </div>
+        )}
+        {!hideButtons && (
+          <div className="buttons d-flex justify-content-end">
+            <div
+              className="btn btn-outline-primary mx-1"
+              onClick={onAddExpenseClick}
+            >
+              Add Expense
+            </div>
+            <div
+              className="btn btn-outline-secondary mx-1"
+              onClick={onViewExpensesClick}
+            >
+              View Expenses
+            </div>
+            {name != "Uncategorized" && budgetId !== UNCATEGORIZED_ID ? (
+              <Button
+                variant="danger"
+                size="sm"
+                className="ms-2"
+                onClick={() => {
+                  removeBudget(budgetId);
+                }}
+              >
+                Delete this Budget
+              </Button>
+            ) : (
+              <></>
+            )}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
